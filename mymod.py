8ae7d2ca13cda90bbdb9d89e8d4cc4e2efe3d1a9
Caleb Schmidt <cdschmidt1103@gmail.com>
2022-09-14 22:23:55 -0400

    first commit

---
+++ mymod.py
@@ -0,0 +1,15 @@
+def say_hello():
+    print("hello")
+
+def say_goodbye():
+    print("goodbye")
+
+from math import pi
+
+def circle(radius):
+    radius = pi * radius * 2
+    return radius
+
+def area(l, w):
+    a = l * w
+    return a
